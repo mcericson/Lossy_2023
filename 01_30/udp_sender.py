6bc8e6c365cb002b519e7b5dd868c42506127f2a
mcericson <mark.ericson@woodbury.edu>
2023-01-30 13:27:43 -0800

    ghpython

---
+++ 01_30/udp_sender.py
@@ -0,0 +1,29 @@
+#source: https://stackoverflow.com/questions/18743962/python-send-udp-packet
+import socket
+
+count = 0 
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+local_hostname = socket.gethostname()
+local_fqdn = socket.getfqdn()
+udp_ip = socket.gethostbyname(local_hostname)   
+print (udp_ip)
+udp_port = 23456
+
+
+def setup():
+    size(100,100)
+    
+    
+def draw():
+    global sock, count, udp_ip, udp_port
+
+    count += 1
+
+    msg = (str(count)).encode("utf-8")
+    sock.sendto(msg, (udp_ip, udp_port))
+
+
+
+
